d30b13225e61b9a4c5bda5cbcb0bcaff0da1decc
Demaris Ashmore <demiashmore@yahoo.com>
2022-11-29 14:09:15 -0700

    commit tree 74f8485ead54f514bd925b944f62681b930ea114

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "Tan", "rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Black", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "Brown", "rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "Tan", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "Green", "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 3, "F", "Black", "happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, "F", "White", "rowdy", 1);
